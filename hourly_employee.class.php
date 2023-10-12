3e2fbdb68916b764b585fea86235291d26291719
Jon9807 <126832845+Jon9807@users.noreply.github.com>
2023-10-12 11:39:53 -0400

    did some of hourly employee

--- hourly_employee.class.php
+++ hourly_employee.class.php
@@ -5,3 +5,30 @@
  * File : hourly_employee.class.php
  * Description:
  */
+class HourlyEmployee extends Employee{
+    private $wage;
+    private $hours;
+
+    public function __construct($hours, $wage){
+        $this->person = $hours;
+        $this->ssn = $wage;
+    }
+
+    public function getWage() {
+        return $this->wage;
+    }
+
+    public function getHours() {
+        return $this->hours;
+    }
+
+    public function getPaymentAmount() {
+        return $this->paymentAmount;
+    }
+
+    public function toString() {
+        
+
+    }
+
+}
